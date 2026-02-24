2a48060ce584752fb0dfa22a77768356d1174c62
IzabelaEftimova <eftimovaizabela@gmail.com>
2026-02-24 09:52:37 +0100

    Walking skeleton - Angular

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. // Middleware
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
